b9c38140d40c169a4de5209d3a3e6ca65d4ca4e1
Raphael Mulenda <mulendaraphael@yahoo.fr>
2022-02-15 23:30:45 +0100

    Branch class created and updated

--- src/Raphael/Mulenda/Branch.java
+++ src/Raphael/Mulenda/Branch.java
@@ -1,6 +1,7 @@
 package Raphael.Mulenda;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Created by Raphael Mulenda on 15/Feb/2022.
@@ -20,5 +21,39 @@ public class Branch {
 
     public ArrayList<Customer> getCustomers() {
         return customers;
+    }
+
+    public boolean newCustomer(String name,double initCash){
+        if (findCustomer(name) ==null){
+            Customer newCustomer = new Customer(name,initCash);
+            this.customers.add(newCustomer);
+            System.out.println("New Customer added Successfully");
+            return true;
+        }
+        System.out.println(name + " exist in contacts");
+        return false;
+
+    }
+    public boolean addCustomerTransaction(String name, double newTransaction){
+        if (findCustomer(name)!=null){
+            Customer theCustomer = findCustomer(name);
+            assert theCustomer != null;
+            theCustomer.addTransaction(newTransaction);
+            return true;
+        }
+        System.out.println(name + " does not exist in customers directory");
+        return false;
+    }
+
+    private  Customer findCustomer(String customerName){
+        Iterator<Customer> lc = this.customers.iterator();
+        while(lc.hasNext()){
+            if (lc.next().getName().equals(customerName)){
+                return lc.next();
+            }
+
+        }
+        return null;
+
     }
 }
