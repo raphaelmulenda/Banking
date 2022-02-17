ed0b00377fda589e8a27b78488ce1280c9288cef
Raphael Mulenda <mulendaraphael@yahoo.fr>
2022-02-17 20:06:46 +0100

    update bank class

--- src/Raphael/Mulenda/Bank.java
+++ src/Raphael/Mulenda/Bank.java
@@ -15,28 +15,71 @@ public class Bank {
         this.branches = new ArrayList<Branch>();
     }
 
-    public boolean addBranch(String branchName){
+    public boolean addBranch(String branchName) {
         Branch checkedBranch = findBranch(branchName);
-        if (checkedBranch == null){
+        if (checkedBranch == null) {
             Branch newBranch = new Branch(branchName);
             this.branches.add(newBranch);
             System.out.println(branchName + " has been haded to database");
             return true;
 
         }
-        System.out.println(branchName + " Exist database");
+        System.out.println(branchName + " Exist in database");
         return false;
 
     }
 
+    public boolean addCustomer(String branchName, String customerName, double initCash) {
+        Branch branch = findBranch(branchName);
+        if (branch != null) {
+            return branch.newCustomer(customerName, initCash);
+
+        }
+        System.out.println(branchName + " Does not exist in database");
+        return false;
+    }
+
+    public boolean addCustomerTransaction(String branchName, String customerName, double newCash) {
+        Branch branch = findBranch(branchName);
+        if (branch != null) {
+            if (branch.addCustomerTransaction(customerName, newCash)) {
+                System.out.println("Transaction Successful");
+                return true;
+            }
+        }
+        System.out.println(branchName + " Does not exist in database");
+        return false;
+    }
 
 
-    private Branch findBranch(String branchName){
-        for (Branch branches : this.branches){
-            if (branches.getName().equals(branchName)){
+    private Branch findBranch(String branchName) {
+        for (Branch branches : this.branches) {
+            if (branches.getName().equals(branchName)) {
                 return branches;
             }
         }
         return null;
     }
+
+    public boolean listCustomer(String branchName, boolean showTransaction) {
+        Branch branch = findBranch(branchName);
+        if (branch != null) {
+            ArrayList<Customer> customers = branch.getCustomers();
+            System.out.println("Customer details for branch " + branch.getName());
+            for (int i = 0; i < customers.size(); i++) {
+                System.out.println("Customer: " + customers.get(i).getName() + "[" + (i + 1) + "]");
+                if (showTransaction) {
+                    System.out.println("Transactions");
+                    System.out.println("[" + (i + 1) + "]" + " Amount: " + customers.get(i).getTransactions());
+
+                }
+
+
+            }
+            return true;
+
+        }
+        return false;
+    }
 }
+
